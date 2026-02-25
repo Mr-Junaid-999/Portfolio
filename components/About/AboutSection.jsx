523f2100f6d3df57601f1dceb2d3869a7a140c67
Mr-Junaid-999 <mr.junaidulhassan@gmail.com>
2026-02-26 01:56:52 +0500

    adjust about content

--- components/About/AboutSection.jsx
+++ components/About/AboutSection.jsx
@@ -6,13 +6,13 @@ export default async function AboutSection() {
       id="about"
       className="py-20 bg-gradient-to-br from-gray-50 to-blue-50"
     >
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-[1280px] mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div className=" mb-16">
           <h2 className="text-4xl md:text-5xl font-bold text-gray-800 mb-4 text-center">
             About Me
           </h2>
-          <p className="text-lg md:text-xl lg:text-xl text-gray-600 max-w-3xl mx-auto   text-center">
+          <p className="text-lg md:text-xl lg:text-xl text-gray-600 max-w-6xl mx-auto   text-center">
             <strong>Full-Stack Developer (Next.js / React / Node.js)</strong>{" "}
             with a strong foundation in backend logic and system design. My
             journey began with C++, where I developed multiple desktop
@@ -24,9 +24,10 @@ export default async function AboutSection() {
             work on both WordPress and Next.js projects. I develop custom
             WordPress themes, while simultaneously building modern web
             applications with Next.js 16 (App Router) and Supabase. <br />
+            <br />
             <strong>Key Achievement:</strong>
           </p>
-          <ul className="list-disc list-inside mt-2 text-left max-w-2xl mx-auto text-center">
+          <ul className="text-lg md:text-xl lg:text-xl  list-disc list-inside mt-2 text-left max-w-6xl mx-auto text-gray-600">
             <li>
               Built a production-ready e-commerce platform from scratch using
               Next.js 16, Supabase, and Server Actions.
@@ -48,7 +49,7 @@ export default async function AboutSection() {
               Created RESTful API routes for file uploads and media management.
             </li>
           </ul>
-          <p className="text-lg md:text-xl lg:text-xl text-gray-600 max-w-3xl mx-auto text-start pt-5">
+          <p className="text-lg md:text-xl lg:text-xl text-gray-600 max-w-6xl mx-auto text-start pt-5">
             <strong>Technical Skills:</strong> Next.js, React, Node.js,
             TypeScript (learning), Supabase, PostgreSQL, MongoDB , WordPress,
             Tailwind CSS, Git, Server Actions, REST APIs
